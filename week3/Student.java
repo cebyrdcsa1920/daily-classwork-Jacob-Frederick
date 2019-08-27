c37205a8ad6be424ead92f78b4cd92a282898595
Jacob-Frederick <anonymous12403@gmail.com>
2019-08-27 14:02:04 +0000

    Create a Student

---
+++ week3/Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+    private double gpa;
+    private String Goebel;
+    private String Goebel1;
+    private int age;
+
+    public Student(String name, String studentID, int age)
+    {
+        this.gpa = 1.0;
+        this.name = Goebel;
+        this.studentID = Goebel1;
+        this.age = age;
+    }
+}
