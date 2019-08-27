404ce3b5766aff4ac3aa56d3b03bdf28ca3b9244
Jacob-Frederick <anonymous12403@gmail.com>
2019-08-27 14:11:02 +0000

    Student creation

--- week3/Student.java
+++ week3/Student.java
@@ -8,8 +8,14 @@ public class Student
     public Student(String name, String studentID, int age)
     {
         this.gpa = 1.0;
-        this.name = Goebel;
-        this.studentID = Goebel1;
+        this.Goebel = Goebel;
+        this.Goebel1 = Goebel1;
         this.age = age;
+    }
+
+    public static void main(String[] args)
+    {
+        Student Goebel = new Student("Goebel", "GoebelStinky", 2);
+        System.out.println(Goebel);
     }
 }
