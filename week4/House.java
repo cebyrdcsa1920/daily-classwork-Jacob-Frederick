b6152f3ee544b333e212716d4fa17cb572f1e18d
Jacob-Frederick <anonymous12403@gmail.com>
2019-09-05 14:11:45 +0000

    Uh Oh! Stinky House!

---
+++ week4/House.java
@@ -0,0 +1,20 @@
+public class House
+{
+    private boolean big;
+    private boolean runningWater;
+    private boolean indians;
+    private int numRooms;
+    private boolean stinky;
+    private String name;
+
+    public House()
+    {
+        big = true;
+        runningWater = false;
+        indians = true;
+        numRooms = 1;
+        stinky = true;
+        name = "TajMaStinky";
+    }
+
+}
---
+++ week4/HouseBuilder.java
@@ -0,0 +1,8 @@
+public class HouseBuilder
+{
+    public static void main(String[] args)
+    {
+        House stinkyHouse = new House();
+        System.out.println(stinkyHouse);
+    }
+}
